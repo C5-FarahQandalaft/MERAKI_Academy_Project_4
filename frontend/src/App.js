f43ff8fea51bc9656914bed769a7b0bb9ced604b
FarahQandalaft <qandalaft.f@gmail.com>
2022-05-12 12:13:06 +0300

    0-preparing FE section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 
+// import routes
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Routes></Routes>
     </div>
   );
 }
