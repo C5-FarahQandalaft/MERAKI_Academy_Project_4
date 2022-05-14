3c1a71fe4f53df0989fb38bba71d7c877ecfa99d
FarahQandalaft <qandalaft.f@gmail.com>
2022-05-14 22:52:53 +0300

    8-deletePostById FE

--- frontend/src/components/Jobs/index.js
+++ frontend/src/components/Jobs/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect, useContext } from "react";
 import "./style.css";
-import { FiEdit } from "react-icons/fi";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 import { postIdContext } from "../../App";
@@ -45,99 +45,126 @@ const AllJobs = ({ token }) => {
     setSearchTitle(e.target.value);
   };
 
+  //delete post
+  const deletePost = (id) => {
+    axios
+      .delete(`http://localhost:5000/jobs/delete/post/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(() => {
+        getAllJobs();
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+      });
+  };
+
   //useEffect to show all posts
   useEffect(() => {
     getAllJobs();
   }, []);
 
   return (
-    <div className="mainContainer">
+    <div className="Container">
       <input placeholder="Search" onChange={seachByTitle} />
-      {error ? (
-        <p>{error}</p>
-      ) : posts ? (
-        posts
-          .filter((el) => {
-            return (
-              el.title.includes(searchTitle) ||
-              el.company.name.includes(searchTitle)
-            );
-          })
-          .map((element) => {
-            return (
-              <div key={element._id} className="post">
-                <div className="postHeader">
-                  <h2>{element.company.name}</h2>
+      <div className="mainContainer">
+        {error ? (
+          <p>{error}</p>
+        ) : posts ? (
+          posts
+            .filter((el) => {
+              return (
+                el.title.includes(searchTitle) ||
+                el.company.name.includes(searchTitle)
+              );
+            })
+            .map((element) => {
+              return (
+                <div key={element._id} className="post">
+                  <div className="postHeader">
+                    <h2>{element.company.name}</h2>
 
-                  {typeOfUser === "company" ? (
-                    company === element.company.name ? (
-                      <div id={element._id} className="controlDiv">
-                        <FiEdit
-                          id={element._id}
-                          className="edit"
-                          onClick={(e) => {
-                            const id = e.target.id;
-                            showUpdateForm(id);
-                          }}
-                        />
-                      </div>
+                    {typeOfUser === "company" ? (
+                      company === element.company.name ? (
+                        <div id={element._id} className="controlDiv">
+                          <FiEdit
+                            id={element._id}
+                            className="edit"
+                            onClick={(e) => {
+                              const id = e.target.id;
+                              showUpdateForm(id);
+                            }}
+                          />
+
+                          <FiTrash2
+                            id={element._id}
+                            className="delete"
+                            onClick={(e) => {
+                              const id = e.target.id;
+                              deletePost(id);
+                            }}
+                          />
+                        </div>
+                      ) : (
+                        <></>
+                      )
                     ) : (
                       <></>
-                    )
-                  ) : (
-                    <></>
-                  )}
-                </div>
+                    )}
+                  </div>
 
-                <div className="info">
-                  <h3>Job title : </h3>
-                  <p>{element.title}</p>
-                </div>
+                  <div className="info">
+                    <h3>Job title : </h3>
+                    <p>{element.title}</p>
+                  </div>
 
-                <div className="info">
-                  <h3>Job description : </h3>
-                  <p>{element.description}</p>
-                </div>
+                  <div className="info">
+                    <h3>Job description : </h3>
+                    <p>{element.description}</p>
+                  </div>
 
-                <div className="info">
-                  <h3>Experience : </h3>
-                  <p>{element.experience}</p>
-                </div>
+                  <div className="info">
+                    <h3>Experience : </h3>
+                    <p>{element.experience}</p>
+                  </div>
 
-                <div className="info">
-                  <h3>Salary : </h3> <p>{element.salary}</p>
-                </div>
+                  <div className="info">
+                    <h3>Salary : </h3> <p>{element.salary}</p>
+                  </div>
 
-                <div className="info">
-                  <h3>Job type : </h3>
-                  <p>{element.type}</p>
-                </div>
+                  <div className="info">
+                    <h3>Job type : </h3>
+                    <p>{element.type}</p>
+                  </div>
 
-                <div className="info">
-                  <h3>Location : </h3>
-                  <p>{element.location}</p>
-                </div>
+                  <div className="info">
+                    <h3>Location : </h3>
+                    <p>{element.location}</p>
+                  </div>
 
-                <div className="info">
-                  <h3>Appliacants : </h3>
-                  <p>{element.applicants.length}</p>
-                </div>
+                  <div className="info">
+                    <h3>Appliacants : </h3>
+                    <p>{element.applicants.length}</p>
+                  </div>
 
-                <div className="info">
-                  <h3>Remote : </h3>
-                  <p>{element.remote ? "Yes" : "No"}</p>
-                </div>
+                  <div className="info">
+                    <h3>Remote : </h3>
+                    <p>{element.remote ? "Yes" : "No"}</p>
+                  </div>
 
-                <div className="info">
-                  <h3>Available : </h3>
-                  <p>{element.available ? "Yes" : "No"}</p>
+                  <div className="info">
+                    <h3>Available : </h3>
+                    <p>{element.available ? "Yes" : "No"}</p>
+                  </div>
                 </div>
-              </div>
-            );
-          })
-      ) : (
-        "No jobs found"
-      )}
+              );
+            })
+        ) : (
+          "No jobs found"
+        )}
+      </div>
     </div>
   );
 };
